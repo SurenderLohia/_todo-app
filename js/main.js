7579a233a009063f1a49022e3bba5862f1f99468
Surender Lohia <surender.lohia.e@gmail.com>
2020-08-07 08:43:46 +0530

    Render TodoList

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-console.log('Todo App');
+// IIFE to create local scope and to avoid global namespacing collision
+(function () {
+  const todoCollection = {
+    "1": {
+      id: 1,
+      task: "HTML",
+      isCompleted: false,
+    },
+    "2": {
+      id: 1,
+      task: "CSS",
+      isCompleted: false,
+    },
+  };
+
+  const todoIds = [1, 2];
+
+  function createListItemEl(todoItem) {
+    return `<li class="flex align-center todo-list-item">
+    <input class="mt0 mb0 mr1" type="checkbox" value="${todoItem.id}" id="${todoItem.id}" />
+    <label for="${todoItem.id}">${todoItem.task}</label>
+    </li>`;
+  }
+
+  function renderTodoList(container, todoCollection) {
+    let todoListHTML = "";
+    todoIds.forEach((id) => {
+      todoListHTML += createListItemEl(todoCollection[id]);
+    });
+
+    container.innerHTML = todoListHTML;
+  }
+
+  // Dom Elements
+  const todoListEl = document.getElementById("js-todo-list");
+
+  renderTodoList(todoListEl, todoCollection);
+})();
